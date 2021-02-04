206860dfee7ef48eee30366a631b44f2bb502be8
James Sullivan <jsullivan86@gmail.com>
2021-02-04 15:44:55 -0800

    Almost a real game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,7 +7,22 @@ let gameStatus = '';
 window.addEventListener('DOMContentLoaded', event => {
     let board = document.getElementById('tic-tac-toe-board');
 
+    let newGame = document.getElementById('new-game');
+
+    newGame.addEventListener('click', event => {
+        currentPlayerSymbol = 'x';
+        svArr = ['', '', '', '', '', '', '', '', ''];
+        gameStatus = '';
+        document.getElementById('game-status').innerHTML = '';
+        for (let i = 0; i < 9; i++) {
+            let divSquare = document.getElementById(`square-${i}`);
+            divSquare.innerHTML = '';
+        }
+        newGame.disabled = true;
+    })
+
     board.addEventListener('click', event => {
+        if (gameStatus !== '') return;
         let target = event.target.id;
         let num;
         let image = document.createElement('img');
@@ -35,7 +50,6 @@ window.addEventListener('DOMContentLoaded', event => {
             checkGameStatus();
             currentPlayerSymbol = 'x';
         }
-        // checkGameStatus();
 
     });
 
@@ -46,37 +60,38 @@ window.addEventListener('DOMContentLoaded', event => {
 let checkGameStatus = () => {
     // rows
     if (svArr[0] === svArr[1] && svArr[1] === svArr[2] && svArr[2] !== '') {
-        // do something
         gameStatus = currentPlayerSymbol.toUpperCase();
-        console.log(gameStatus);
     }
     if (svArr[3] === svArr[4] && svArr[4] === svArr[5] && svArr[5] !== '') {
-        // do something
         gameStatus = currentPlayerSymbol.toUpperCase();
     }
     if (svArr[6] === svArr[7] && svArr[7] === svArr[8] && svArr[8] !== '') {
-        // do something
         gameStatus = currentPlayerSymbol.toUpperCase();
     }
 
     // columns
     if (svArr[0] === svArr[3] && svArr[3] === svArr[6] && svArr[6] !== '') {
-        // do something
         gameStatus = currentPlayerSymbol.toUpperCase();
-        console.log(gameStatus);
     }
     if (svArr[1] === svArr[4] && svArr[4] === svArr[7] && svArr[7] !== '') {
-        // do something
         gameStatus = currentPlayerSymbol.toUpperCase();
     }
     if (svArr[2] === svArr[5] && svArr[5] === svArr[8] && svArr[8] !== '') {
-        // do something
         gameStatus = currentPlayerSymbol.toUpperCase();
     }
 
-    // d
-
-    if (gameStatus) {
-        document.getElementById('game-status').innerHTML = 'Winner: ' + gameStatus;
+    // diagonal
+    if (svArr[0] === svArr[4] && svArr[4] === svArr[8] && svArr[8] !== '') {
+        gameStatus = currentPlayerSymbol.toUpperCase();
     }
+    if (svArr[2] === svArr[4] && svArr[4] === svArr[6] && svArr[6] !== '') {
+        gameStatus = currentPlayerSymbol.toUpperCase();
+    }
+
+    if (gameStatus !== '') {
+        document.getElementById('game-status').innerHTML = 'Winner: ' + gameStatus;
+        let newGame = document.getElementById('new-game');
+        newGame.disabled = false;
+    }
+
 }
