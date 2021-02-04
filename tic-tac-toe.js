b63174f4bee426f388922195e44cc9d6574208d8
James Sullivan <jsullivan86@gmail.com>
2021-02-04 14:14:31 -0800

    images working sort of

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,16 +3,33 @@ let squareValues = ['', '', '', '', '', '', '', '', ''];
 
 window.addEventListener('DOMContentLoaded', event => {
     let board = document.getElementById('tic-tac-toe-board');
+    let image = document.createElement('img');
 
     board.addEventListener('click', event => {
         let target = event.target.id;
-
+        let num;
         if (!target.includes('square-')) {
             return;
         } else {
-            target.slice(target.indexOf('-') + 1);
+            num = Number.parseInt(target.slice(target.indexOf('-') + 1));
+
         }
 
+        if (squareValues[num] !== '') {
+            return;
+        }
+        if (currentPlayerSymbol === 'x') {
+            image.src = 'images/player-x.svg';
+            event.target.appendChild(image);
+            squareValues[num] = currentPlayerSymbol;
+            currentPlayerSymbol = 'o';
+        } else {
+            image.src = 'images/player-o.svg';
+            event.target.appendChild(image);
+            squareValues[num] = currentPlayerSymbol;
+            currentPlayerSymbol = 'x';
+        }
+        console.log(squareValues);
     })
 
 });
